73576b3ba6608fd1a038dd89874f264927bc9150
tpraissa <raissatelep@gmail.com>
2020-12-29 09:21:09 -0300

    Incluindo o Swagger.

---
+++ CL.Core/Domain/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CL.Core.Domain
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
---
+++ CL_WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,55 @@
+﻿using CL.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        //public IEnumerable<Cliente> Get()
+        public IActionResult Get ()
+        {
+            return Ok(new List<Cliente>() { 
+                new Cliente { Id = 1, Nome = "Erick Moreira", DataNascimento = new DateTime(1995,04,11)},
+                new Cliente { Id = 2, Nome = "Raissa Teles", DataNascimento = new DateTime(1993,04,13)}
+            });
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
